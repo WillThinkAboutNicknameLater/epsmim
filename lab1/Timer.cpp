c729534f53e575ed80e7743f322c8ce62745d213
Cookie <59281475+WillThinkAboutNicknameLater@users.noreply.github.com>
2022-02-12 22:12:21 +0700

    Update Timer.cpp

--- lab1/Timer.cpp
+++ lab1/Timer.cpp
@@ -5,7 +5,7 @@
 #include "Timer.h"
 
 std::chrono::duration<double> Timer::measure(const std::function<void()> &function) {
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start{ std::chrono::high_resolution_clock::now() };
     function();
     return std::chrono::high_resolution_clock::now() - start;
 }
